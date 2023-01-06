957694c910a9c7e24d0729c34756c13480f40587
Soumya0587 <ghoshalsoumalya96@gmail.com>
2023-01-06 16:35:03 +0530

    new

--- next.config.js
+++ next.config.js
@@ -1,17 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // images:{
-  //   unoptimized : true,
-  //   remotePatterns : [
-  //     {
-  //       protocol : "https",
-  //       hostname : "images-na.ssl-images-amazon.com",
-  //       port : "",
-  //       pathname : "**"
-  //     },
-  //   ],
-  // },
+  
 }
 
 module.exports = nextConfig
